84d441b93c6f989c95c962cb31bf4c10f6e2bd01
jammer <adityathakur00199.8@gmail.com>
2026-09-20 00:52:13 +0530

    first commit

---
+++ src/config/Db.js
@@ -0,0 +1,19 @@
+import pg from "pg";
+import logger from "./utils/logger.js";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URI,
+});
+
+pool.on("connect", () => {
+  logger.info("PostgreSQL pool: new client connected");
+});
+
+pool.on("error", (err) => {
+  logger.error({ err }, "Unexpected error on idle PostgreSQL client");
+  process.exit(1);
+});
+
+export default pool;
